b4394402d7ad3ce173f311178ec76bb89dc2e715
trachanh1609 <Vu@Users-MacBook-Pro.local>
2018-06-06 15:59:45 +0300

    completed part II

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,13 +17,30 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import configureStore from './store';
+
+import {Route, Switch} from 'react-router'
+import Login from './containers/Login';
+import PrivateRoute from './containers/PrivateRoute';
+
 const history = createHistory();
 const store = configureStore(history);
+
+
+// ReactDOM.render((
+//   <Provider store={store}>
+//     <ConnectedRouter history={history}>
+//       <App />
+//     </ConnectedRouter>
+//   </Provider>
+// ), document.getElementById('root'));
 
 ReactDOM.render((
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <Switch>
+        <Route exact path="/login/" component={Login} />
+        <PrivateRoute path="/" component={App}/>
+      </Switch>
     </ConnectedRouter>
   </Provider>
 ), document.getElementById('root'));
