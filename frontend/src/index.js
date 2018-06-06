7d2d279c8a219708ce51e8bd5170a810cf14b29c
trachanh1609 <Vu@Users-MacBook-Pro.local>
2018-06-06 14:06:38 +0300

    init frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
+import configureStore from './store';
+const history = createHistory();
+const store = configureStore(history);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+), document.getElementById('root'));
